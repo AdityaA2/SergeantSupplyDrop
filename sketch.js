194bcd23c1d2e578d4950a86e23e57a1b1b5cb29
AdityaA2 <71200213+AdityaA2@users.noreply.github.com>
2020-10-25 19:57:10 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
-var packageBody,ground
+var packageBody,ground;
+var box1,box2,box3;
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
@@ -37,7 +38,23 @@ function setup() {
 
 	//Create a Ground
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
- 	World.add(world, ground);
+	 World.add(world, ground);
+	 
+	 boxPosition=width/2-100;
+	boxY=610;
+	boxleftSprite=createSprite(boxPosition, boxY, 20,100); 
+	boxleftSprite.shapeColor=color(255,0,0); 
+	boxLeftBody = Bodies.rectangle(boxPosition+20, boxY, 20,100 , {isStatic:true} );
+	 World.add(world, boxLeftBody); 
+	 boxBase=createSprite(boxPosition+100, boxY+40, 200,20);
+	  boxBase.shapeColor=color(255,0,0); 
+	  boxBottomBody = Bodies.rectangle(boxPosition+100, boxY+45-20, 200,20 , {isStatic:true} );
+	   World.add(world, boxBottomBody); 
+	   boxleftSprite=createSprite(boxPosition+200 , boxY, 20,100); 
+	   boxleftSprite.shapeColor=color(255,0,0); 
+	   boxRightBody = Bodies.rectangle(boxPosition+200-20 , boxY, 20,100 , {isStatic:true} ); 
+	   World.add(world, boxRightBody);
+	 
 
 
 	Engine.run(engine);
